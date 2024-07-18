32dade5db1b12e01ca37c4faff0202986cb4fbdf
facusteckler86 <facundomsteckler@gmail.com>
2024-07-17 21:18:55 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { CoursesComponent } from './features/dashboard/courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "auth",
+    component: LoginComponent,
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    children: [
+      {
+        path: "courses",
+        component: CoursesComponent
+      }
+    ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
